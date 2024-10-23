2f09d29cbecb853c8c57d29cf5ef28e6d78fc8df
liylo <2813164552@qq.com>
2024-10-23 18:59:16 +0800

    fix decoder

--- decoder.sv
+++ decoder.sv
@@ -42,50 +42,89 @@ module DECODER #(
     output rf_waddr
 );
     always_comb begin
+
         case(opcode)
-            7b0110111: begin
-                imm = U_imm;
+            // LUI
+            7'b0110111: begin
+                imm = {U_imm, 12'b0}; // Upper immediate
                 imm_type = 4'b1000;
+                rf_raddr_a = 5'b0;
+                rf_raddr_b = 5'b0;
+                rf_waddr = rd;
+            end
+            // AUIPC
+            7'b0010111: begin
+                imm = {U_imm, 12'b0}; // Upper immediate
+                imm_type = 4'b1000;
+                rf_raddr_a = 5'b0;
+                rf_raddr_b = 5'b0;
+                rf_waddr = rd;
+            end
+            // JAL
+            7'b1101111: begin
+                imm = {UJ_imm, 12'b0}; // Upper immediate
+                imm_type = 4'b1001;
+                rf_raddr_a = 5'b0;
+                rf_raddr_b = 5'b0;
+                rf_waddr = rd;
+            end
+            // JALR
+            7'b1100111: begin
+                imm = I_imm;
+                imm_type = 4'b1000;
+                rf_raddr_a = rs1;
+                rf_raddr_b = 5'b0;
+                rf_waddr = rd;
+            end
+            // LOAD
+            7'b0000011: begin
+                imm = I_imm;
+                imm_type = 4'b1000;
+                rf_raddr_a = rs1;
+                rf_raddr_b = 5'b0;
+                rf_waddr = rd;
+            end
+            // IMM
+            7'b0010011: begin
+                imm = I_imm;
+                imm_type = 4'b1000;
+                rf_raddr_a = rs1;
+                rf_raddr_b = 5'b0;
+                rf_waddr = rd;
+            end
+            // STORE
+            7'b0100011: begin
+                imm = S_imm;
+                imm_type = 4'b1010;
+                rf_raddr_a = rs1;
+                rf_raddr_b = rs2;
+                rf_waddr = 5'b0;
+            end
+            // BRANCH
+            7'b1100011: begin
+                imm = SB_imm;
+                imm_type = 4'b1100;
+                rf_raddr_a = rs1;
+                rf_raddr_b = rs2;
+                rf_waddr = 5'b0;
+            end
+            // OP
+            7'b0110011: begin
+                imm = 0;
+                imm_type = 4'b0000; // R-type has no immediate
                 rf_raddr_a = rs1;
                 rf_raddr_b = rs2;
                 rf_waddr = rd;
             end
-            7b0010111: begin
-                imm = U_imm;
-                imm_type = 4'b1000;
-                rf_raddr_a = rs1;
-                rf_raddr_b = 0;
-                rf_waddr = rd;
-            end
-            7b1101111: begin
-                imm = UJ_imm;
-                imm_type = 4'b1001;
-                rf_raddr_a = rs1;
-                rf_raddr_b = 0;
-                rf_waddr = rd;
-            end
-            7b1100111: begin
-                imm = I_imm;
-                imm_type = 4'b1000;
-                rf_raddr_a = rs1;
-                rf_raddr_b = 0;
-                rf_waddr = rd;
-            end
-            7b0000011: begin
-                imm = I_imm;
-                imm_type = 4'b1000;
-                rf_raddr_a = rs1;
-                rf_raddr_b = 0;
-                rf_waddr = rd;
-            end
-            7b0010011: begin
-                imm = I_imm;
-                imm_type = 4'b1000;
-                rf_raddr_a = rs1;
-                rf_raddr_b = 0;
-                rf_waddr = rd;
+            default: begin
+                imm = 0;
+                imm_type = 0;
+                rf_raddr_a = 5'b0;
+                rf_raddr_b = 5'b0;
+                rf_waddr = 5'b0;
             end
         endcase
     end
+
 
 endmodule
